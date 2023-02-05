f5cae8f950d50d1cd80679bb8ab24acdf346584e
Jeff <60796676+JeffNeff@users.noreply.github.com>
2023-02-05 10:41:55 -0700

    Update fresh.sh

--- cmd/partychaind/fresh.sh
+++ cmd/partychaind/fresh.sh
@@ -1,7 +1,8 @@
 rm -rf ~/.partychain
+go build . 
 ./partychaind init party 
 ./partychaind keys add alice  --keyring-backend test 
 ./partychaind add-genesis-account alice 5000000000000000000000stake 
 ./partychaind gentx alice  100000000stake   --keyring-backend test
 ./partychaind collect-gentxs
-go run .  start --log_level error
+./partychaind start --log_level error
